17f3f27b7b3d84f98647620a772e22c6770cc7a5
Ivan Caldarella <gamein3x@gmail.com>
2026-03-31 16:40:54 +0200

    Primo ragionamento

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 `use strict`;
+
+/// Palidroma
+
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+//- Creo una funzione per verificare il palindromo, da legare al prompt;
+//- La funzione deve ritornare "false" se :
+//      -Inserisci un carattere che non sia una lettera (inclusi spazi);
+//- La funzione deve:
+//      -Convertire lettere maiuscole in minuscole;
+//      -Dividere la stringa in un array che contiene tutte le lettere della parola;
+//      -Leggere l'array a contrario e verificare che corrisponda all'array in ordine;
+//      -Ritornare true se non corrisponde e false se corrisponde;
+
+/// Pari e Dispari
+
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.,
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).,
+// Sommiamo i due numeri,
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione),
+// Dichiariamo chi ha vinto.
