72169772a084a1e92a255ccb492ae34c1df3ec22
Ivan Caldarella <gamein3x@gmail.com>
2026-03-31 18:22:12 +0200

    Gioco funziona, esercizio finito

--- pariDispari/script1.js
+++ pariDispari/script1.js
@@ -13,17 +13,45 @@
 //- Sommo e controllo il risultato con modulo
 
 
-
-function winCondCheck() {
-    if 
+// Creo una funzione per controllare che l'utente abbia inserito "pari" o "dispari" all'inizio del ciclo
+function winCondCheck(par) {
+    return par === "pari" || par === "dispari";
 }
 
+// Funzione per numero random da 1 a 5
 function random5() {
     return Math.floor(Math.random() * 5) + 1;
 }
 
-const inputWinCond = prompt('Scrivi "Pari" o "Dispari" per scegliere');
-const inputNum = parseInt(prompt('Scegli un numero da 1 a 5'));
-const CPUNum = random5();
+// Funzione per controllo se numero finale è pari o dispari
+function isEven(num) {
+    return num % 2 === 0;
+}
 
-const sum = CPUNum + inputNum;
+const inputWinCond = prompt('Scrivi "Pari" o "Dispari" per scegliere').toLowerCase();
+
+if (!winCondCheck(inputWinCond)) {
+    alert('Devi scrivere "pari" o "dispari". Ricarica la pagina e riprova.');
+} else {
+    const inputNum = parseInt(prompt("Scegli un numero da 1 a 5"));
+
+    // controllo numero valido
+    if (isNaN(inputNum) || inputNum < 1 || inputNum > 5) {
+        alert("Devi inserire un numero tra 1 e 5. Ricarica e riprova.");
+    } else {
+        const CPUNum = random5();
+        const sum = CPUNum + inputNum;
+        const sumIsEven = isEven(sum);
+        const result = sumIsEven ? "pari" : "dispari";
+
+        alert(
+            `Hai scelto ${inputWinCond}, tu hai giocato ${inputNum}, il computer ${CPUNum}. La somma è ${sum} ed è ${result}.`
+        );
+
+        if (result === inputWinCond) {
+            alert("Hai vinto!");
+        } else {
+            alert("Hai perso!");
+        }
+    }
+}
