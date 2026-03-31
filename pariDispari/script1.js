d2564e5fb5e879f940014945112754994943e924
Ivan Caldarella <gamein3x@gmail.com>
2026-03-31 17:57:38 +0200

    Basi del secondo gioco

--- pariDispari/script1.js
+++ pariDispari/script1.js
@@ -7,3 +7,23 @@
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+
+//- Creo due variabili che contengano i prompt dell'user e trasformino in interi;
+//- Creo una funzione per generare un numero da 1 a 5 per la CPU;
+//- Sommo e controllo il risultato con modulo
+
+
+
+function winCondCheck() {
+    if 
+}
+
+function random5() {
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+const inputWinCond = prompt('Scrivi "Pari" o "Dispari" per scegliere');
+const inputNum = parseInt(prompt('Scegli un numero da 1 a 5'));
+const CPUNum = random5();
+
+const sum = CPUNum + inputNum;
